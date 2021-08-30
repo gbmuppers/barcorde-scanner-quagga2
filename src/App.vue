4133ec2873724746c5d666472221d96fbb400683
Gibran Ojeda <16161306@itoaxaca.edu.mx>
2021-08-30 10:35:52 -0500

    dist

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="table-responsive" style="margin-top:0rem;">
+      <button class="btn btn-outline-primary btn-block" @click="reset()">Reset</button>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Intento</th>
+            <th>Codigo</th>
+            <th>Formato</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(scanner,index) in list_decoded_barcode" :key="index">
+            <td>{{index}}</td>
+            <td>{{scanner.codigo}}</td>
+            <td>{{scanner.formato}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="container" style="margin-bottom:1rem;">
+      <Scanner :onDetected="logIt" :readerSize="readerSize" :readerTypes="['code_128_reader','ean_reader','ean_8_reader','code_39_reader','code_39_vin_reader','codabar_reader','upc_reader','upc_e_reader','i2of5_reader','2of5_reader','code_93_reader']"></Scanner>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Scanner from './components/Scanner.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Scanner
+  },
+  data () {
+    return {
+      list_decoded_barcode:[],
+      readerSize: {
+        width: 640,
+        height: 480
+      },
+      detecteds: []
+    }
+  },
+  methods: {
+    logIt (data) {
+      //console.log('detected', data)
+      this.list_decoded_barcode.push({codigo:data.codeResult.code,formato:data.codeResult.format})
+    },
+    reset(){
+      this.list_decoded_barcode = []
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Scanner.vue
@@ -0,0 +1,167 @@
+<template>
+  <div id="interactive" class="viewport scanner">
+    <video />
+    <canvas class="drawingBuffer" />
+  </div>
+</template>
+
+<script>
+import Quagga from '@ericblade/quagga2';
+
+export default {
+  props: {
+    onDetected: {
+      type: Function,
+      default(result) {
+        console.log('detected: ', result);
+      },
+    },
+    onProcessed: {
+      type: Function,
+      default(result) {
+        let drawingCtx = Quagga.canvas.ctx.overlay;
+
+        let drawingCanvas = Quagga.canvas.dom.overlay;
+
+        if (result) {
+          if (result.boxes) {
+            drawingCtx.clearRect(
+              0,
+              0,
+              parseInt(drawingCanvas.getAttribute('width')),
+              parseInt(drawingCanvas.getAttribute('height'))
+            );
+            result.boxes
+              .filter(function(box) {
+                return box !== result.box;
+              })
+              .forEach(function(box) {
+                Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+                  color: '#F00',
+                  lineWidth: 3,
+                });
+              });
+          }
+          if (result.box) {
+            if (this.locate){
+              Quagga.ImageDebug.drawPath(
+                result.box,
+                { x: 0, y: 1 },
+                drawingCtx,
+                {
+                  color: '#00F',
+                  lineWidth: 3,
+              });
+            }
+          }
+
+          if (result.codeResult && result.codeResult.code) {
+            Quagga.ImageDebug.drawPath(
+              result.line,
+              { x: 'x', y: 'y' },
+              drawingCtx,
+              { color: '#FF0', lineWidth: 4 }
+            );
+          }
+        }
+      },
+    },
+    readerTypes: {
+      type: Array,
+      default: () => ['code_128_reader'],
+    },
+    readerSize: {
+      type: Object,
+      default: () => ({
+        width: 640,
+        height: 480,
+      }),
+      validator: o =>
+        typeof o.width === 'number' && typeof o.height === 'number',
+    },
+    aspectRatio: {
+      type: Object,
+      default: () => ({
+        min: 1,
+        max: 2,
+      }),
+      validator: o => typeof o.min === 'number' && typeof o.max === 'number',
+    },
+    locate: {
+      type: Boolean,
+      default: () => false,
+    },
+    area: {
+      type: Object,
+      default: () => ({
+        top: '30%',
+        right: '30%',
+        left: '30%',
+        bottom: '30%',
+      }),
+    },
+  },
+  data: function() {
+    return {
+      quaggaState: {
+        inputStream: {
+          type: 'LiveStream',
+          constraints: {
+            width: { min: this.readerSize.width },
+            height: { min: this.readerSize.height },
+            facingMode: 'environment',
+            aspectRatio: {
+              min: this.aspectRatio.min,
+              max: this.aspectRatio.max,
+            },
+          },
+          area: {
+            top: this.area.top,
+            right: this.area.right,
+            left: this.area.left,
+            bottom: this.area.bottom,
+          },
+        },
+        locator: {
+          patchSize: 'medium',
+          halfSample: true,
+        },
+        numOfWorkers: 2,
+        frequency: 10,
+        decoder: {
+          readers: this.readerTypes,
+        },
+        multiple:true,
+        locate: this.locate,
+      },
+    };
+  },
+  mounted: function() {
+    Quagga.init(this.quaggaState, function(err) {
+      if (err) {
+        return console.error(err);
+      }
+      Quagga.start();
+    });
+    Quagga.onDetected(this.onDetected);
+    Quagga.onProcessed(this.onProcessed);
+  },
+  destroyed: function() {
+    Quagga.stop();
+  },
+};
+</script>
+
+<style scoped>
+.viewport {
+  position: relative;
+}
+
+.viewport canvas,
+.viewport video {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+}
+</style>
